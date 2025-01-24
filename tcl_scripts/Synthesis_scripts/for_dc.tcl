d96601f65419ac8faa48750c31ccea0ac54ecb78
Sudipta22-umn <sudipta.eee22@gmail.com>
2025-01-24 10:45:05 -0600

    Add files via upload

---
+++ tcl_scripts/Synthesis_scripts/for_dc.tcl
@@ -0,0 +1,23 @@
+set rtl_all {   \
+    ./rtl/serial_adder.v \
+    ./rtl/PE_TOP_4.v \
+    ./rtl/MERGE_PE_TOP.v \
+    ./rtl/PE_TOP_6.v \
+    ./rtl/MAC_5.v \
+    ./rtl/PE_ROW_6.v \
+    ./rtl/MAC_6.v \
+    ./rtl/PE_4.v \
+    ./rtl/NZ_GEN_6.v \
+    ./rtl/PE_TOP_5.v \
+    ./rtl/PE_ARRAY.v \
+    ./rtl/MERGE_PE_COMB.v \
+    ./rtl/log2.vh \
+    ./rtl/MAC_4.v \
+    ./rtl/PE_ROW_4.v \
+    ./rtl/NZ_GEN_4.v \
+    ./rtl/NZ_GEN_5.v \
+    ./rtl/PE_ROW_5.v \
+    ./rtl/PE_6.v \
+    ./rtl/priority_encoder.v \
+    ./rtl/PE_5.v \
+}
